716b6296ee151dc3c1e95506ad106456ade594b2
Phi Pham <phipham882@gmail.com>
2024-11-14 14:20:09 +0700

    Update UI and Done functuon forget password

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,7 +1,8 @@
 import 'package:booking_place/global.dart';
 import 'package:booking_place/view/signup_screen.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -101,6 +102,20 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                   TextButton(
                     onPressed: () {
+                      _showForgotPasswordDialog(); // Hiển thị hộp thoại quên mật khẩu
+                    },
+                    child: const Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
                       Get.to(SignupScreen());
                     },
                     child: const Text(
@@ -123,5 +138,115 @@ class _LoginScreenState extends State<LoginScreen> {
         ],
       ),
     ));
+  }
+
+  _showForgotPasswordDialog() {
+    TextEditingController emailController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text(
+            'Reset Password',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.deepPurple,
+            ),
+          ),
+          content: TextField(
+            controller: emailController,
+            decoration: InputDecoration(
+              hintText: 'Enter your email',
+              hintStyle: TextStyle(color: Colors.deepPurpleAccent),
+              border: OutlineInputBorder(),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.deepPurple, width: 2),
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                'Cancel',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurpleAccent,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor:
+                    Colors.deepPurple, // Background color of the button
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: const Text(
+                'Send Reset Link',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () async {
+                String email = emailController.text.trim();
+                if (email.isNotEmpty && email.contains("@")) {
+                  await _resetPassword(email);
+                  Navigator.of(context).pop();
+                } else {
+                  Get.snackbar(
+                    "Error",
+                    "Please enter a valid email.",
+                    backgroundColor: Colors.red,
+                    colorText: Colors.white,
+                    snackPosition: SnackPosition.BOTTOM,
+                    duration: Duration(seconds: 3),
+                    borderRadius: 8,
+                    margin: EdgeInsets.all(10),
+                    icon: Icon(Icons.error, color: Colors.white),
+                  );
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _resetPassword(String email) async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+
+      Get.snackbar(
+        "Password Reset",
+        "A password reset link has been sent to your email.",
+        backgroundColor: Colors.green,
+        colorText: Colors.white,
+        snackPosition: SnackPosition.BOTTOM,
+        duration: Duration(seconds: 3),
+        borderRadius: 8,
+        margin: EdgeInsets.all(10),
+        icon: Icon(Icons.email, color: Colors.white),
+      );
+    } catch (e) {
+      Get.snackbar(
+        "Error",
+        e.toString(),
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        snackPosition: SnackPosition.BOTTOM,
+        duration: Duration(seconds: 3),
+        borderRadius: 8,
+        margin: EdgeInsets.all(10),
+        icon: Icon(Icons.error, color: Colors.white),
+      );
+    }
   }
 }
